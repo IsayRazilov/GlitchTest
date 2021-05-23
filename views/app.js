c0e9069a92380df9db426c9c4291b5f30fcde292
Isay Razilov <isay.razilov@gmail.com>
2021-05-23 23:13:33 +0300

    Adding all for simple website.

--- views/app.js
+++ views/app.js
@@ -1,14 +1,5 @@
 $(document).ready(() => {
 
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
-
-    $('#give').click(() => {
-        // console.log('Clicked button')
-        // $.post('/give', { amount: '100' })
-    })
 
     
 })
